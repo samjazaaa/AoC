51378fe7271d8ef79351d4a247241b8198686fcf
Felix Knoerr <felix.knoerr@gmx.de>
2023-12-08 21:04:14 +0100

    2023/07-2

--- 2023/07/joker.py
+++ 2023/07/joker.py
@@ -1,7 +1,7 @@
 import sys
 from functools import cmp_to_key
 
-order = ["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"][::-1]
+order = ["A", "K", "Q", "T", "9", "8", "7", "6", "5", "4", "3", "2", "J"][::-1]
 
 
 def parse_hands(lines):
@@ -15,41 +15,95 @@ def parse_hands(lines):
 
 
 def is_five(cards):
-    return all(map(lambda c: c == cards[0], cards))
+    if not "J" in cards:
+        return all(map(lambda c: c == cards[0], cards))
+
+    cleaned = cards.replace("J", "")
+    return cleaned == "" or all(map(lambda c: c == cleaned[0], cleaned))
 
 
 def is_four(cards):
-    return cards.count(cards[0]) == 4 or cards.count(cards[1]) == 4
+    if not "J" in cards:
+        return cards.count(cards[0]) == 4 or cards.count(cards[1]) == 4
+
+    jokers = cards.count("J")
+    cleaned = cards.replace("J", "")
+
+    if jokers >= 3:
+        return True
+
+    if jokers == 2:
+        return is_pair(cleaned)
+
+    # only 1 Joker
+    return is_three(cleaned)
 
 
 def is_full_house(cards):
-    sorted_cards = sorted(cards)
-    first_count = sorted_cards.count(sorted_cards[0])
-    second_count = sorted_cards.count(sorted_cards[4])
-    return (first_count == 3 and second_count == 2) or (
-        first_count == 2 and second_count == 3
-    )
+    sorted_cards = "".join(sorted(cards))
+
+    if not "J" in cards:
+        first_count = sorted_cards.count(sorted_cards[0])
+        second_count = sorted_cards.count(sorted_cards[4])
+        return (first_count == 3 and second_count == 2) or (
+            first_count == 2 and second_count == 3
+        )
+
+    jokers = sorted_cards.count("J")
+    cleaned = sorted_cards.replace("J", "")
+
+    if jokers >= 3:
+        return True
+
+    if jokers == 2:
+        return is_three(cleaned) or is_pair(cleaned)
+
+    # only 1 Joker
+    return is_three(cleaned) or is_double_pair(cleaned)
 
 
 def is_three(cards):
-    for c in cards:
-        if cards.count(c) == 3:
-            return True
-    return False
+    if not "J" in cards:
+        for c in cards:
+            if cards.count(c) == 3:
+                return True
+        return False
+
+    jokers = cards.count("J")
+    cleaned = cards.replace("J", "")
+
+    if jokers >= 2:
+        return True
+
+    # only 1 Joker
+    return is_pair(cleaned)
 
 
 def is_double_pair(cards):
-    first_symbol = ""
-    for c in cards:
-        if cards.count(c) == 2:
-            if first_symbol == "" or first_symbol == c:
-                first_symbol = c
-            else:
-                return True
-    return False
+    if not "J" in cards:
+        first_symbol = ""
+        for c in cards:
+            if cards.count(c) == 2:
+                if first_symbol == "" or first_symbol == c:
+                    first_symbol = c
+                else:
+                    return True
+        return False
+
+    jokers = cards.count("J")
+    cleaned = cards.replace("J", "")
+
+    if jokers >= 2:
+        return True
+
+    # only 1 Joker
+    return is_pair(cleaned)
 
 
 def is_pair(cards):
+    if "J" in cards:
+        return True
+
     for c in cards:
         if cards.count(c) == 2:
             return True
